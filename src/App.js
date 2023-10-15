a6b9b2a7c06b1b3ec34b2d2c88c068b1c9e6cd77
Shrishail Patil <91820178+shrishail356@users.noreply.github.com>
2023-10-15 22:16:28 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,3 @@
-/*import React from 'react';
-import './App.css';
-import ColorPicker from './components/ColorPicker';
-
-function App() {
-  const colors = ["#FF5733", "#33FF68", "#337BFF", "#FFE833", "#FF33E8"];
-
-  return (
-    <div className="App">
-      <ColorPicker colors={colors} />
-    </div>
-  );
-}
-
-export default App;
-*/
-/*import React from 'react';
-import './App.css';
-import UserTable from '../src/components/UserTable';
-
-function App() {
-  return (
-    <div className="App">
-      <UserTable />
-    </div>
-  );
-}
-
-export default App;
-*/
 import React from 'react';
 import './App.css';
 import AgeCalculator from '../src/components/AgeCalculator';
